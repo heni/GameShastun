3fe70ae5a92491ff066d85a918d944dc135a25a6
Eugene Krokhalev <eugene.krokhalev@gmail.com>
2024-11-11 16:53:52 +0300

    refactor code

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ type PrecalcSkip struct {
 	speedup       float64
 	moves_to_skip []Move
 }
+type TimedMove struct {
+	time_to_win float64
+	move        Move
+}
 
 func update_state_1(s BoardState, m Move) (BoardState, bool) {
 	x := (1 << (m[0] - 1)) | (1 << (m[1] - 1))
@@ -77,11 +81,6 @@ func calc_game_table(strategy StrategyOptions, skips_table *[]PrecalcSkip) []flo
 	time_to_win[0] = 0.0
 	for _, pos := range calculation_queue[1:] {
 
-		type TimedMove struct {
-			time_to_win float64
-			move        Move
-		}
-
 		next_moves := make([]TimedMove, 0)
 		skips := 0
 
@@ -107,36 +106,17 @@ func calc_game_table(strategy StrategyOptions, skips_table *[]PrecalcSkip) []flo
 			}
 		}
 
-		slices.SortFunc(next_moves, func(a, b TimedMove) int {
-			return int(math.Copysign(1, a.time_to_win-b.time_to_win))
-		})
-
-		sum_tm, skip_tm := 0.0, 0.0
-		extra_skips := 0
+		sum_tm := 0.0
 		for _, move := range next_moves {
 			sum_tm += move.time_to_win
 		}
-
 		best_time := (36 + sum_tm) / (36 - float64(skips))
 
 		if strategy.do_optimal_skip {
-			speedup_time := 0.0
-			moves_to_skip := make([]Move, 0)
-
-			for i := len(next_moves) - 1; i > 0; i-- {
-				skip_tm += next_moves[i].time_to_win
-				extra_skips++
-				cand_time := (36 + sum_tm - skip_tm) / (36 - float64(skips+extra_skips))
-				if cand_time > best_time {
-					break
+			if speedup_time, moves_to_skip := prune_bad_moves(next_moves, &best_time); moves_to_skip != nil {
+				if skips_table != nil {
+					(*skips_table)[pos] = PrecalcSkip{speedup_time, moves_to_skip}
 				}
-
-				moves_to_skip = append(moves_to_skip, next_moves[i].move)
-				speedup_time += best_time - cand_time
-				best_time = cand_time
-			}
-			if len(moves_to_skip) > 0 && skips_table != nil {
-				(*skips_table)[pos] = PrecalcSkip{speedup_time, moves_to_skip}
 			}
 		}
 
@@ -144,6 +124,40 @@ func calc_game_table(strategy StrategyOptions, skips_table *[]PrecalcSkip) []flo
 	}
 
 	return time_to_win
+}
+
+func prune_bad_moves(moves []TimedMove, best_time *float64) (float64, []Move) {
+	slices.SortFunc(moves, func(a, b TimedMove) int {
+		return int(math.Copysign(1, a.time_to_win-b.time_to_win))
+	})
+
+	skips := 36 - len(moves)
+	sum_tm := 0.0
+	for _, move := range moves {
+		sum_tm += move.time_to_win
+	}
+
+	skip_tm, speedup_time := 0.0, 0.0
+	extra_skips := 0
+	moves_to_skip := make([]Move, 0)
+	for i := len(moves) - 1; i > 0; i-- {
+		skip_tm += moves[i].time_to_win
+		extra_skips++
+		cand_time := (36 + sum_tm - skip_tm) / (36 - float64(skips+extra_skips))
+		if cand_time > *best_time {
+			break
+		}
+
+		moves_to_skip = append(moves_to_skip, moves[i].move)
+		speedup_time += *best_time - cand_time
+		*best_time = cand_time
+	}
+
+	if len(moves_to_skip) > 0 {
+		return speedup_time, moves_to_skip
+	}
+
+	return 0.0, nil
 }
 
 func save_game_table(game []float64, filename string) {

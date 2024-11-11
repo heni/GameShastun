92b0ae9b28ca27087a90d58c7b702e9ca1c97a41
Eugene Krokhalev <eugene.krokhalev@gmail.com>
2024-11-11 16:09:23 +0300

    add options to calculate game with enforces "skip move" option

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"slices"
+	"strings"
 )
 
 type BoardState int
 type Move [2]int
+type StrategyOptions struct {
+	do_optimal_selection bool
+	do_optimal_skip      bool
+}
+type PrecalcSkip struct {
+	speedup       float64
+	moves_to_skip []Move
+}
 
 func update_state_1(s BoardState, m Move) (BoardState, bool) {
 	x := (1 << (m[0] - 1)) | (1 << (m[1] - 1))
@@ -34,7 +44,24 @@ func nbits(v int) int {
 	return k
 }
 
-func calc_game_table() []float64 {
+func state_to_string(s BoardState) string {
+	var builder strings.Builder
+	builder.WriteString("{")
+	pos := int(s)
+	for p := 1; p <= 12; p++ {
+		if pos&(1<<(p-1)) != 0 {
+			if pos >= 1<<p {
+				builder.WriteString(fmt.Sprintf("%d,", p))
+			} else {
+				builder.WriteString(fmt.Sprintf("%d", p))
+			}
+		}
+	}
+	builder.WriteString("}")
+	return builder.String()
+}
+
+func calc_game_table(strategy StrategyOptions, skips_table *[]PrecalcSkip) []float64 {
 	const N = 1 << 12
 
 	time_to_win := make([]float64, N)
@@ -49,24 +76,71 @@ func calc_game_table() []float64 {
 
 	time_to_win[0] = 0.0
 	for _, pos := range calculation_queue[1:] {
-		d, t := 1.0, 1.0
+
+		type TimedMove struct {
+			time_to_win float64
+			move        Move
+		}
+
+		next_moves := make([]TimedMove, 0)
+		skips := 0
+
 		for a := 1; a <= 6; a++ {
 			for b := 1; b <= 6; b++ {
 				s1, ok1 := update_state_1(BoardState(pos), Move{a, b})
 				s2, ok2 := update_state_2(BoardState(pos), Move{a, b})
 				if ok1 && ok2 {
-					t += min(time_to_win[int(s1)], time_to_win[int(s2)]) / 36.0
-					// t += (.5*time_to_win[int(s1)] + .5*time_to_win[int(s2)]) / 36.0
+					var tm float64
+					if strategy.do_optimal_selection {
+						tm = min(time_to_win[int(s1)], time_to_win[int(s2)])
+					} else {
+						tm = .5*time_to_win[int(s1)] + .5*time_to_win[int(s2)]
+					}
+					next_moves = append(next_moves, TimedMove{tm, Move{a, b}})
 				} else if ok1 {
-					t += time_to_win[int(s1)] / 36.0
+					next_moves = append(next_moves, TimedMove{time_to_win[int(s1)], Move{a, b}})
 				} else if ok2 {
-					t += time_to_win[int(s2)] / 36.0
+					next_moves = append(next_moves, TimedMove{time_to_win[int(s2)], Move{a, b}})
 				} else {
-					d -= 1 / 36.0
+					skips++
 				}
 			}
 		}
-		time_to_win[pos] = t / d
+
+		slices.SortFunc(next_moves, func(a, b TimedMove) int {
+			return int(math.Copysign(1, a.time_to_win-b.time_to_win))
+		})
+
+		sum_tm, skip_tm := 0.0, 0.0
+		extra_skips := 0
+		for _, move := range next_moves {
+			sum_tm += move.time_to_win
+		}
+
+		best_time := (36 + sum_tm) / (36 - float64(skips))
+
+		if strategy.do_optimal_skip {
+			speedup_time := 0.0
+			moves_to_skip := make([]Move, 0)
+
+			for i := len(next_moves) - 1; i > 0; i-- {
+				skip_tm += next_moves[i].time_to_win
+				extra_skips++
+				cand_time := (36 + sum_tm - skip_tm) / (36 - float64(skips+extra_skips))
+				if cand_time > best_time {
+					break
+				}
+
+				moves_to_skip = append(moves_to_skip, next_moves[i].move)
+				speedup_time += best_time - cand_time
+				best_time = cand_time
+			}
+			if len(moves_to_skip) > 0 && skips_table != nil {
+				(*skips_table)[pos] = PrecalcSkip{speedup_time, moves_to_skip}
+			}
+		}
+
+		time_to_win[pos] = best_time
 	}
 
 	return time_to_win
@@ -78,25 +152,52 @@ func save_game_table(game []float64, filename string) {
 		panic("can't create output file")
 	}
 
-	defer f.Close()
 	buffer := bufio.NewWriter(f)
+	defer func() {
+		buffer.Flush()
+		f.Close()
+	}()
+
 	for pos := range game {
-		buffer.WriteString("{")
-		for p := 1; p <= 12; p++ {
-			if pos&(1<<(p-1)) != 0 {
-				if pos >= 1<<p {
-					buffer.WriteString(fmt.Sprintf("%d,", p))
-				} else {
-					buffer.WriteString(fmt.Sprintf("%d", p))
+		buffer.WriteString(fmt.Sprintf("%s\t%.5f\n", state_to_string(BoardState(pos)), game[pos]))
+	}
+}
+
+func save_skips_table(skips []PrecalcSkip, game []float64, filename string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		panic("can't create output file")
+	}
+
+	buffer := bufio.NewWriter(f)
+	defer func() {
+		buffer.Flush()
+		f.Close()
+	}()
+
+	for pos, skip_op := range skips {
+		if skip_op.moves_to_skip != nil {
+
+			moves_to_skip := make([]Move, 0)
+			for _, m := range skip_op.moves_to_skip {
+				if m[0] <= m[1] { // don't print move twice with it's mirror
+					moves_to_skip = append(moves_to_skip, m)
 				}
 			}
+			slices.Reverse(moves_to_skip)
+
+			buffer.WriteString(fmt.Sprintf(
+				"%s\tskip moves:%v; speedup:%.4g=>%.4g\n",
+				state_to_string(BoardState(pos)), moves_to_skip, game[pos]+skip_op.speedup, game[pos],
+			))
 		}
-		buffer.WriteString(fmt.Sprintf("}\t%.5f\n", game[pos]))
 	}
-	buffer.Flush()
 }
 
 func main() {
-	game := calc_game_table()
-	save_game_table(game, "game_optimal.txt")
+	strategy := StrategyOptions{true, true}
+	skips := make([]PrecalcSkip, 1<<12)
+	game := calc_game_table(strategy, &skips)
+	save_game_table(game, "game_optimal[do_skips].txt")
+	save_skips_table(skips, game, "do_skips.txt")
 }
